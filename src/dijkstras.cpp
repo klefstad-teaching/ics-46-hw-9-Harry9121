b5a6fe4c40de72dcac7f9f66835d20d7102d432a
Harry9121 <harryliu0524@gmail.com>
2025-03-12 20:26:15 +0000

    first commit of HW9

---
+++ src/dijkstras.cpp
@@ -0,0 +1,43 @@
+#include "dijkstras.h"
+
+vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+    int n = G.numVertices;
+    vector<int> distance(n, INF);
+    vector<bool> visited(n, false);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    distance[source] = 0;
+    pq.push({0, source});
+    previous.resize(n, -1);
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        pq.pop();
+        if (visited[u]) continue;
+        visited[u] = true;
+        for (const Edge& edge : G[u]) {
+            int v = edge.dst;
+            int weight = edge.weight;
+            if (!visited[v] && distance[u] + weight < distance[v]) {
+                distance[v] = distance[u] + weight;
+                previous[v] = u;
+                pq.push({distance[v], v});
+            }
+        }
+    }
+    return distance;
+}
+
+vector<int> extract_shortest_path(const vector<int>& distances, const vector<int>& previous, int destination) {
+    vector<int> path;
+    for (int v = destination; v != -1; v = previous[v]) {
+        path.push_back(v);
+    }
+    reverse(path.begin(), path.end());
+    return path;
+}
+
+void print_path(const vector<int>& v, int total) {
+    for (size_t i = 0; i < v.size(); ++i) {
+        cout << v[i] << (i + 1 < v.size() ? " " : "\n");
+    }
+    cout << "Total cost is " << total << endl;
+}
---
+++ src/ladder_main.cpp
@@ -0,0 +1,9 @@
+#include "ladder.h"
+
+int main() {
+    set<string> word_list;
+    load_words(word_list, "src/words.txt");
+    vector<string> ladder = generate_word_ladder("cat", "dog", word_list);
+    print_word_ladder(ladder);
+    verify_word_ladder();
+}
